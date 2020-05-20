0563bbb3aa24b05b21de35a63d25cccd83585f68
Макс <@WIN-26JG68Q4EA3>
2020-05-20 12:02:31 +0300

    создание дела

--- VDCompany/Models/Entitys/StartContext.cs
+++ VDCompany/Models/Entitys/StartContext.cs
@@ -12,6 +12,7 @@ namespace VDCompany.Models.Entitys
 
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Case> Cases { get; set; }
+        public DbSet<Create> Creates { get; set; }
         public DbSet<Doc> Docs { get; set; }
         public DbSet<Dialog> Dialogs { get; set; }
         public DbSet<Lawyer> Lawyers { get; set; }
