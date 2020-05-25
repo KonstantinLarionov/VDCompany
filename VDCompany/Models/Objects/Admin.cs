efd04027c905daf4f55f8cdf9752efb828a904e5
Макс <@WIN-26JG68Q4EA3>
2020-05-25 14:23:03 +0300

    контакты

--- VDCompany/Models/Objects/Admin.cs
+++ VDCompany/Models/Objects/Admin.cs
@@ -10,6 +10,7 @@ namespace VDCompany.Models.Objects
         public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public List<Image> Image { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string FIO { get; set; }
