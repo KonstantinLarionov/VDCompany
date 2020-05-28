e06fd20e4ed7a9c3992e49d196a64f429ae352b5
Макс <@WIN-26JG68Q4EA3>
2020-05-28 08:35:58 +0300

    кейс

--- VDCompany/Models/Objects/Case.cs
+++ VDCompany/Models/Objects/Case.cs
@@ -11,7 +11,7 @@ namespace VDCompany.Models.Objects
     }
     public class Case
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; }
         public string Description { get; set; }
         public string Type { get; set; }
