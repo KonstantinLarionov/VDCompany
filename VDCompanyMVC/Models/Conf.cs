6af523977e5e7c84e9b2290a4f73003caf2a170d
Константин Ларионов <kostya12277@yandex.ru>
2021-07-22 14:15:21 +0500

    ~~

--- VDCompanyMVC/Models/Conf.cs
+++ VDCompanyMVC/Models/Conf.cs
@@ -8,11 +8,11 @@ namespace VDCompanyMVC.Models
     public static class Conf
     {
 #if DEBUG
-        public static string ConnectDb = "Server=localhost;Database=u0967433_vd;User=u0967_vd;Password=$lI2bg26;";
-        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_vd_chat;User=u0967_vd;Password=$lI2bg26;";
+        public static string ConnectDb = "Server=localhost;Database=u0967433_vd;User=root;Password=root;";
+        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_vd_chat;User=root;Password=root;";
 #else
-        public static string ConnectDb = "Server=localhost;Database=u0967433_vd;User=u0967_vd;Password=$lI2bg26;";
-        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_vd_chat;User=u0967_vd;Password=$lI2bg26;";
+        public static string ConnectDb = "Server=localhost;Database=u0967433_vd;User=u0967433_u0967_vd;Password=Rqzs34#2;";
+        public static string DBChat { get; private set; } = "Server=localhost;Database=u0967433_vd_chat;User=u0967433_u0967_vd;Password=Rqzs34#2;";
 #endif
     }
 }
